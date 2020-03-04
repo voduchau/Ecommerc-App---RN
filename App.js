69c1e2f867fe331a171dffacc575e8d09083ac66
voduchau <voduchau01011997@gmail.com>
2020-03-04 17:30:44 +0700

    Create Setting Screen

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import HomeScreen from './src/screens/HomeScreen';
 import ProfileScreen from './src/screens/ProfileScreen';
+import SettingScreen from './src/screens/SettingScreen';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Icon from 'react-native-vector-icons/FontAwesome';
@@ -12,18 +13,31 @@ const App = () => {
   return(
     <NavigationContainer>
       <Tab.Navigator>
-      <Tab.Screen name="HomeScreen" component={HomeScreen} options={{
+
+        {/* Home Screen */}
+        <Tab.Screen name="HomeScreen" component={HomeScreen} options={{
           tabBarLabel: 'Home',
           tabBarIcon: ({focused, color, size }) =>( 
            focused ? <AntDesign name="home" size={30} color="blue" /> : <AntDesign name="home" size={30} color="gray" />
           ),
         }} />
+
+        {/* Profile Screen */}
         <Tab.Screen name="ProfileScreen" component={ProfileScreen} options={{
           tabBarLabel: 'Profile',
           tabBarIcon: ({focused, color, size }) => (
             focused? <AntDesign name="profile" size={30} color="blue" /> : <AntDesign name="profile" size={30} color="gray" />
           ),
         }} />
+
+        {/* Setting Screen */}
+        <Tab.Screen name="SettingScreen" component={SettingScreen} options={{
+          tabBarLabel: 'Setting Screen',
+          tabBarIcon: ({focused, color, size }) => (
+            focused? <AntDesign name="setting" size={30} color="blue" /> : <AntDesign name="setting" size={30} color="gray" />
+          ),
+        }} />
+        
       </Tab.Navigator>
     </NavigationContainer>
   );
