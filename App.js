67d73d5f1a38220981be39ca0cc8bc56e487eb5f
voduchau <voduchau01011997@gmail.com>
2020-03-04 16:24:59 +0700

    InputComponent

--- App.js
+++ App.js
@@ -1,10 +1,31 @@
 import React from 'react';
 import HomeScreen from './src/screens/HomeScreen';
+import ProfileScreen from './src/screens/ProfileScreen';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+import { View,Text} from 'react-native';
 
-
+const Tab = createBottomTabNavigator();
 const App = () => {
   return(
-    <HomeScreen />
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen  name="HomeScreen" component={HomeScreen} options={{
+          tabBarLabel: 'Home',
+          tabBarIcon: ({ color, size }) => (
+              <Icon name="home" size={30} color="#900" />
+          ),
+        }} />
+        <Tab.Screen name="ProfileScreen" component={ProfileScreen} options={{
+          tabBarLabel: 'Profile',
+          tabBarIcon: ({ color, size }) => (
+            <AntDesign name="profile" size={30} color="#900" />
+          ),
+        }} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 };
 
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { View, Text, StyleSheet} from 'react-native';
+import InputComponent from '../components/InputComponent';
 
 const HomeScreen = () => {
     return (
         <View>
+            <InputComponent />
             <Text>This is HomeScreen</Text>
         </View>
     );
