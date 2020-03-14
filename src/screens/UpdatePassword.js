fd20cdf8380166442442fba5840de802dba26f4f
voduchau <voduchau01011997@gmail.com>
2020-03-14 17:21:04 +0700

    fix reauthenticate pass word

--- src/screens/UpdatePassword.js
+++ src/screens/UpdatePassword.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import Firebase from '../../config/Firebase';
 // import firebase from 'react-native-firebase';
+import * as firebase from 'firebase';
 import {View,Text,StyleSheet, Button} from 'react-native';
 import { TextInput } from 'react-native-gesture-handler';
 import { set } from 'react-native-reanimated';
@@ -13,25 +14,27 @@ const UpdatePassword = ({navigation})=>{
     const [newPassword,setNewPassword] = useState('');
     const [err,setErr] = useState('');
 
-    const changePassword = async (newPassword) => {
-      const user = Firebase.auth().currentUser
-      try {
-        await user.updatePassword(newPassword)
-        navigation.navigate('LoginScreen')
-      } catch(err){
-        console.log(err);
-        setErr(err.message);
-      }
+
+    const reauthenticate = (currentPassword)=>{
+      let user = Firebase.auth().currentUser;
+      let cred = firebase.auth.EmailAuthProvider.credential(user.email,currentPassword);
+      return user.reauthenticateWithCredential(cred);
     }
-    const SignOut = async ()=>{
-      try {
-          await Firebase.auth().signOut();
-          console.log('ok');
-          navigation.navigate('LoginScreen')
-      } catch (e) {
-          console.log(e);
-      }
-  }
+
+    const changePassword =  (newPassword,currentPassword) => {
+     reauthenticate(currentPassword).then(()=>{
+      let user = Firebase.auth().currentUser;
+      user.updatePassword(newPassword).then(()=>{
+        console.log('update pass succes');
+      })
+      .catch((error)=>{
+        console.log(error.messgae);
+      })
+     })
+     .catch((error)=>{
+       console.log(error.messgae);
+     })
+    }
 
     return (
         <View>
@@ -43,13 +46,14 @@ const UpdatePassword = ({navigation})=>{
                     placeholder='New Password'
                     secureTextEntry={true}
                 />
-                {err != ''?
-                <>
-                  <Text>Please Login again before update pass word</Text>
-                  <Button title='Login' onPress={()=>SignOut()} />
-                  </>
-                :null}
-            <Button title='change pass' onPress={()=>changePassword(newPassword)} />
+                <TextInput
+                    style={styles.inputBox}
+                    value={currentPassword}
+                    onChangeText={password => setCurrentPassword(password)}
+                    placeholder='current Password'
+                    secureTextEntry={true}
+                />
+            <Button title='change pass' onPress={()=>changePassword(newPassword,currentPassword)} />
             
         </View>
     )
